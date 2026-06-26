927c7cd2313d5fdccbb773d094a7503449701349
jiang_glb <3345678_glb@gmail.com>
2026-06-26 21:05:13 +0800

    帳號功能-使用MD5加密

--- src/main/java/com/jlaker/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/jlaker/springbootmall/service/impl/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
 
 @Component
@@ -36,6 +37,8 @@ public class UserServiceImpl implements UserService {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
 
+        String hashedPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashedPassword);
 
         return userDao.createUser(userRegisterRequest);
     }
@@ -48,7 +51,11 @@ public class UserServiceImpl implements UserService {
             log.warn("該 email {}尚未註冊", userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        if (user.getPassword().equals(userLoginRequest.getPassword())) {
+
+        String hashedPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+
+        if (user.getPassword().equals(hashedPassword)) {
             return user;
         } else {
             log.warn("email{}的密碼不正確", userLoginRequest.getEmail());
