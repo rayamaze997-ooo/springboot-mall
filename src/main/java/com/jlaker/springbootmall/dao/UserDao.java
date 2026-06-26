8bebb38f252a5a42e9411919e9e04a8823944a49
jiang_glb <3345678_glb@gmail.com>
2026-06-26 14:12:15 +0800

    帳號功能-檢查註冊的 email、隱藏返回的密碼

--- src/main/java/com/jlaker/springbootmall/dao/UserDao.java
+++ src/main/java/com/jlaker/springbootmall/dao/UserDao.java
@@ -8,5 +8,7 @@ public interface UserDao {
 
     User getUserById(Integer userId);
 
+    User getUserByEmail(String email);
+
     Integer createUser(UserRegisterRequest userRegisterRequest);
 }
--- src/main/java/com/jlaker/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/jlaker/springbootmall/service/impl/UserServiceImpl.java
@@ -4,11 +4,17 @@ import com.jlaker.springbootmall.dao.UserDao;
 import com.jlaker.springbootmall.dto.UserRegisterRequest;
 import com.jlaker.springbootmall.model.User;
 import com.jlaker.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
     @Autowired
     private UserDao userDao;
 
@@ -19,6 +25,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+
+        User user =userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+
+        if (user != null) {
+
+            log.warn("email {} 已經被註冊", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+
         return userDao.createUser(userRegisterRequest);
     }
 }
